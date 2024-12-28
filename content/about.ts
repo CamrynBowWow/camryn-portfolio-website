9d2abfcfc52ec1932514c1770d72efdf0aa61bb3
CamrynBowWow <bowdencamryn@gmail.com>
2024-12-28 20:28:14 +0200

    About Page Done

---
+++ content/about.ts
@@ -0,0 +1,238 @@
+export const MainContent: AboutContent = {
+	title: 'Camryn Bowden',
+	startingPoint:
+		"A young developer passionate about programming. Always interested in learning something new and growing new skills. I'm enthusiastic about learning new languages and further deepening my understand of the programming.",
+	experience:
+		"I've been programming for a few years. Learning new stacks and strengthening the knowledge I have learnt. A have year worth of experience in software development. Have required a Bachelors of Science in Information Technology (Software Engineering) and a Higher Certificate in Information Systems (Software Development).",
+	passion:
+		'My choice for has stemmed from a passion of gaming and wishing to make games. From this I leaped into programming and falling in love with all the different aspects of programming.',
+	bestChoice:
+		'I have these no surrender attitude and knowing any problem can be solved if you just put your mind to it.',
+	excitedReason:
+		'The excitement of new and different programming languages has always interested me and how close but uniquely different they are. Its always enjoyable when learning something new and how the differ from existing programming languages.',
+	mainImage: '/SadDoggo.png',
+};
+
+export const LanguagesContent: TechStackContent = {
+	title: 'Programming Languages',
+	content: [
+		{
+			name: 'C#',
+			image: '/assets/languages/cSharp.svg',
+		},
+		{
+			name: 'CSS',
+			image: '/assets/languages/css.svg',
+		},
+		{
+			name: 'C++',
+			image: '/assets/languages/cPlusPlus.svg',
+		},
+		{
+			name: 'JavaScript',
+			image: '/assets/languages/javaScript.svg',
+		},
+		{
+			name: 'Java',
+			image: '/assets/languages/java.svg',
+		},
+		{
+			name: 'PHP',
+			image: '/assets/languages/php.svg',
+		},
+		{
+			name: 'TypeScript',
+			image: '/assets/languages/typeScript.svg',
+		},
+		{
+			name: 'Rust',
+			image: '/assets/languages/rust.svg',
+		},
+		{
+			name: 'Python',
+			image: '/assets/languages/python.svg',
+		},
+	],
+};
+
+export const FrameworksContent: TechStackContent = {
+	title: 'Framework',
+	content: [
+		{
+			name: 'Angular',
+			image: 'assets/frameworks/angular.svg',
+		},
+		{
+			name: 'Blazor',
+			image: 'assets/frameworks/blazor.svg',
+		},
+		{
+			name: 'Dotnetcore', // TODO: fix naming
+			image: 'assets/frameworks/dotnetcore.svg',
+		},
+		{
+			name: 'Fastify',
+			image: 'assets/frameworks/fastify.svg',
+		},
+		{
+			name: 'Flutter',
+			image: 'assets/frameworks/flutter.svg',
+		},
+		{
+			name: 'Next.js',
+			image: 'assets/frameworks/nextjs.svg',
+		},
+		{
+			name: 'React',
+			image: 'assets/frameworks/react.svg',
+		},
+		{
+			name: 'Svelte',
+			image: 'assets/frameworks/svelte.svg',
+		},
+		{
+			name: 'TailwindCSS',
+			image: 'assets/frameworks/tailwindcss.svg',
+		},
+	],
+};
+
+export const DatabasesContent: TechStackContent = {
+	title: 'Databases',
+	content: [
+		{
+			name: 'Appwrite',
+			image: 'assets/databases/appwrite.svg',
+		},
+		{
+			name: 'Firebase',
+			image: 'assets/databases/firebase.svg',
+		},
+		{
+			name: 'MongoDB',
+			image: 'assets/databases/mongoDb.svg',
+		},
+		{
+			name: 'MySQL',
+			image: 'assets/databases/mysql.svg',
+		},
+		{
+			name: 'PostgreSQL',
+			image: 'assets/databases/postgresSql.svg',
+		},
+		{
+			name: 'SQL',
+			image: 'assets/databases/sql.svg',
+		},
+		{
+			name: 'SQLite',
+			image: 'assets/databases/sqLite.svg',
+		},
+		{
+			name: 'Supabase',
+			image: 'assets/databases/supabase.svg',
+		},
+		{
+			name: 'PlanetScale',
+			image: 'assets/databases/planetscale-seeklogo.svg',
+		},
+		{
+			name: 'Neon',
+			image: 'assets/databases/neon-logomark-light-color.svg',
+		},
+	],
+};
+
+export const LibrariesContent: TechStackContent = {
+	title: 'Libraries',
+	content: [
+		{
+			name: 'Bootstrap',
+			image: 'assets/libraries/bootstrap.svg',
+		},
+		{
+			name: 'Framer Motion',
+			image: 'assets/libraries/framerMotion.svg',
+		},
+		{
+			name: 'Material UI',
+			image: 'assets/libraries/materialUi.svg',
+		},
+		{
+			name: 'Socket.IO',
+			image: 'assets/libraries/socketio.svg',
+		},
+		{
+			name: 'Three.js',
+			image: 'assets/libraries/threejs.svg',
+		},
+	],
+};
+
+export const OrmsContent: TechStackContent = {
+	title: 'ORMs',
+	content: [
+		{
+			name: 'Mongoose',
+			image: 'assets/orms/mongoose.svg',
+		},
+		{
+			name: 'Prisma',
+			image: 'assets/orms/prisma.svg',
+		},
+		{
+			name: 'Drizzle',
+			image: 'assets/orms/drizzle.svg',
+		},
+	],
+};
+
+export const OtherContent: TechStackContent = {
+	title: 'Other',
+	content: [
+		{
+			name: 'Docker',
+			image: 'assets/docker.svg',
+		},
+		{
+			name: 'Figma',
+			image: 'assets/figma.svg',
+		},
+		{
+			name: 'Git',
+			image: 'assets/git.svg',
+		},
+		{
+			name: 'GitHub',
+			image: 'assets/github.svg',
+		},
+		{
+			name: 'GraphQL',
+			image: 'assets/graphql.svg',
+		},
+		{
+			name: 'Node.js',
+			image: 'assets/nodejs.svg',
+		},
+		{
+			name: 'Nodemon',
+			image: 'assets/nodemon.svg',
+		},
+		{
+			name: 'Oracle',
+			image: 'assets/oracle.svg',
+		},
+		{
+			name: 'Sass',
+			image: 'assets/sass.svg',
+		},
+		{
+			name: 'WooCommerce',
+			image: 'assets/woocommerce.svg',
+		},
+		{
+			name: 'WordPress',
+			image: 'assets/wordpress.svg',
+		},
+	],
+};
