43c4e25a6f1cc6b3c3005661cf23044630407539
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-10 11:37:12 +0200

    First Phase Done

--- app/(public)/projects/page.tsx
+++ app/(public)/projects/page.tsx
@@ -1,3 +1,12 @@
-export default function ProjectPage() {
-	return <div>page</div>;
+import Container from '@/components/projects/Container';
+import { db } from '@/drizzle/db';
+
+export default async function ProjectsPage() {
+	const projects = await db.query.CodeProjectTable.findMany();
+
+	return (
+		<section className='container'>
+			<Container projects={projects} />
+		</section>
+	);
 }
