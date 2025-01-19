c46c1330bf8fe364ee1565f7f42dd45f64e98217
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-19 13:54:46 +0200

    Fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { formatInTimeZone, getTimezoneOffset } from 'date-fns-tz';
+import { formatInTimeZone } from 'date-fns-tz';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
