356bb6691919a30b42234fcbc6c0161740490ea2
CamrynBowWow <bowdencamryn@gmail.com>
2025-03-08 10:56:01 +0200

    Title Change

--- app/(public)/home/_components/Title.tsx
+++ app/(public)/home/_components/Title.tsx
@@ -5,10 +5,10 @@ export default function Title() {
 				The Marvelous
 			</h1>
 			<h2 className='font-semibold text-3xl x-sm:text-4xl sm:text-5xl md:text-7xl lg:text-8xl 1-xl:text-9xl uppercase tracking-wide transition-1000'>
-				Misadventures
+				Adventures
 			</h2>
 			<p className='text-sm sm:text-lg lg:text-2xl 1-xl:text-4xl font-medium tracking-wide mt-0 sm:mt-1 md:mt-2 transition-1000'>
-				of <span className='font-light tracking-tight capitalize text-gray-600'>unlikely</span>{' '}
+				of a <span className='font-light tracking-tight capitalize text-gray-600'>unique</span>{' '}
 				<span className='uppercase font-semibold'>Dev</span>
 				<span className='font-light'>eloper</span>
 			</p>
